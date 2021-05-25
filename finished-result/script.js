5f550b1806481727492794925487f32573eefc31
Antonio Estela <antonioestela73@gmail.com>
2021-05-25 18:42:25 -0500

    Finished result

--- finished-result/script.js
+++ finished-result/script.js
@@ -1,6 +1,17 @@
 // API
 const API_ENDPOINT = 'https://yesno.wtf/api';
 
+// SELECTORS
+const answerSelector = document.getElementById('answer');
+const inputSelector = document.getElementById('input');
+const buttonSelector = document.getElementById('button');
+const ballSelector = document.getElementById('ball');
+const errorSelector = document.getElementById('error');
+const barSelector = document.querySelector('.input__bar');
+
+// FLAGS
+let isFetching = false;
+
 /**
  * STEPS:
  *
@@ -11,3 +22,63 @@ const API_ENDPOINT = 'https://yesno.wtf/api';
  * 5. Optional: add loading/error states
  *
  */
+
+const setIsFetching = (value) => isFetching = value;
+
+const setButtonState = (state) => {
+    (state)
+    ? buttonSelector.setAttribute('disabled', 'disabled')
+    : buttonSelector.removeAttribute('disabled');
+}
+
+const cleanUp = () => {
+    setTimeout(() => {
+        answerSelector.innerHTML = '';
+        inputSelector.value = '';
+    }, 2000);
+}
+
+const setData = (answer) => {
+    setTimeout(() => {
+        answerSelector.innerHTML = `<p>${answer}</p>`;
+        ballSelector.classList.remove('shake__ball');
+        setIsFetching(false);
+        setButtonState(false);
+        cleanUp(); 
+    }, 1000);
+}
+
+const fetchAnswer = () => {
+
+    setButtonState(true);
+    setIsFetching(true);
+    ballSelector.classList.add('shake__ball');
+
+    fetch(API_ENDPOINT)
+        .then((body) => body.json())
+        .then((data) => setData(data.answer));
+}
+
+const handleKeyEnter = (event) => {
+    if (event.keyCode === 13) {
+        sendAnswer();
+    }
+}
+
+const sendAnswer = () => {
+    if (isFetching) return;
+    if (!inputSelector.value) return handleError();
+
+    fetchAnswer();
+}
+
+const handleError = () => {
+    barSelector.classList.add('shake__ball');
+    errorSelector.innerHTML = 'You need to type your question';
+    setTimeout(() => {
+        errorSelector.innerHTML = '';
+        barSelector.classList.remove('shake__ball');
+    }, 4000);
+}
+
+buttonSelector.addEventListener('click', sendAnswer);
